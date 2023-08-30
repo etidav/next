28fd60633fd60490f8835e2c4b78f1e5cdb530a1
edavid <etienne.david@heuritech.com>
2023-08-30 18:42:15 +0200

    update gridsearch script

--- experiments_reference_dataset.sh
+++ experiments_reference_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
 # weather
-for horizon in 96 192 336 720; do python3 /next/run/reference_dataset/run_NEXT_model.py --main_folder /next/result/weather_horizon$horizon --dataset_path /next/data/reference_dataset/weather/weather.csv --past_dependency 432 --season 144 --horizon $horizon --nb_max_epoch 200 --nb_iteration_per_epoch 50 --learning_rate 0.005 --batch_size 2048 --gpu_number 0 --seed 0; done
+for horizon in 96 192 336 720; do python3 /next/run/reference_dataset/run_NEXT_model.py --main_folder /next/result/weather_horizon$horizon --dataset_path /next/data/reference_dataset/weather/weather.csv --past_dependency 144 --season 144 --horizon $horizon --nb_max_epoch 200 --nb_iteration_per_epoch 50 --learning_rate 0.005 --batch_size 2048 --gpu_number 0 --seed 0; done
 
 # ILI
 for horizon in 24 36 48 60; do python3 /next/run/reference_dataset/run_NEXT_model.py --main_folder /next/result/ili_horizon$horizon --dataset_path /next/data/reference_dataset/illness/national_illness.csv --past_dependency 104 --season 52 --horizon $horizon --nb_max_epoch 200 --nb_iteration_per_epoch 50 --learning_rate 0.005 --batch_size 2048 --gpu_number 0 --seed 0 --small_model; done
--- run/fashion_dataset/run_gridsearch_NEXT_model.py
+++ run/fashion_dataset/run_gridsearch_NEXT_model.py
@@ -94,6 +94,7 @@ if __name__ == "__main__":
                 batch_size=batch_size,
                 model_folder=model_folder,
             )
+            write_json(model_eval.numpy(), os.path.join(model_folder, "elbo_eval.json"))
             if model_eval < best_model_eval:
                 best_model_eval = model_eval
                 best_training_config = {'learning_rate':learning_rate, 'batch_size':batch_size}
